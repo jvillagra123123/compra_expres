7155365f63d0c5fa47f9a6b3b5076a15ef4d23ee
BenjaminValle <148494222+BenjaminValle@users.noreply.github.com>
2025-01-07 20:20:46 -0300

    Creacion de Proyecto

---
+++ compra_expres/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'compra_expres',
+  webDir: 'www'
+};
+
+export default config;
