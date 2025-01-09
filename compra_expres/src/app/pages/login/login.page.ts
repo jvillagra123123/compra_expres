ffa5cfd3f2fd93a3ff1da927eef8e38709e558c0
TOPO0305 <127121279+TOPO0305@users.noreply.github.com>
2025-01-09 13:07:14 -0300

    mejora de validaciones 2.0

--- compra_expres/src/app/pages/login/login.page.ts
+++ compra_expres/src/app/pages/login/login.page.ts
@@ -19,9 +19,11 @@ export class LoginPage implements OnInit {
         '',
         [
           Validators.required,
-          Validators.minLength(8), // Mínimo 8 caracteres
+          Validators.minLength(9), // Mínimo 9 caracteres
           Validators.maxLength(20), // Máximo 20 caracteres
-          Validators.pattern(/^[a-zA-Z0-9@#%!&*/]+$/), // Alfanuméricos + caracteres especiales permitidos
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#%!&*/])[A-Za-z\d@#%!&*/]+$/
+          ), // Al menos una minúscula, una mayúscula, un número y un carácter especial
         ],
       ],
     });
