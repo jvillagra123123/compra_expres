331a85a1e1b0fd4c3eea7259c312382e824e445a
BenjaminValle <148494222+BenjaminValle@users.noreply.github.com>
2025-01-09 16:11:10 -0300

    Creacion pagina perfil y sus validaciones

--- compra_expres/src/app/pages/profile/profile.page.ts
+++ compra_expres/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
   standalone: false,
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
+  profileForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private navCtrl: NavController) {
+    // Crear el formulario del perfil
+    this.profileForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    }, { validator: this.passwordMatchValidator });
+   }
+
+  // Validador para confirmar contraseñas
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword? null : { notMatching: true };
+  }
+  // Editar el perfil
+  onProfile() {
+    if (this.profileForm.valid) {
+      const { firstName, lastName, email, password } = this.profileForm.value;
+      console.log('Perfil:', { firstName, lastName, email, password });
+      // Llamar al servicio de editar perfil (AuthService)
+      this.navCtrl.navigateForward('/inicio');
+    }
+  }
+
+  
 
   ngOnInit() {
   }
