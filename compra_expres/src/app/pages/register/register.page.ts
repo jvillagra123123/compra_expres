3f19c37d4dae697c7ec4c6d19bd8112d5bf92865
BenjaminValle <148494222+BenjaminValle@users.noreply.github.com>
2025-01-09 00:18:46 -0300

    Validacion completada de registro

--- compra_expres/src/app/pages/register/register.page.ts
+++ compra_expres/src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
   standalone: false,
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private navCtrl: NavController) {
+    // Crear el formulario de registro
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  // Validador para confirmar contraseñas
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notMatching: true };
+  }
+
+  // Método para registrar al usuario
+  onRegister() {
+    if (this.registerForm.valid) {
+      const { firstName, lastName, email, password } = this.registerForm.value;
+      console.log('Registro:', { firstName, lastName, email, password });
+      // Llamar al servicio de registro (AuthService)
+      this.navCtrl.navigateForward('/select-profile');
+    }
+  }
+
+  // Navegar a la pagina de login
+  goToLogin() {
+    this.navCtrl.navigateBack('/login');
+  }
+
+   // Navegar a la pagina de select-profile
+   goToSelectProfile(){
+    this.navCtrl.navigateForward('/select-profile');
+   }
 
   ngOnInit() {
   }
