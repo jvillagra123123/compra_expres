9e4429fc2c710c1b7c16de210223cef651924994
BenjaminValle <148494222+BenjaminValle@users.noreply.github.com>
2025-01-11 17:14:29 -0300

    Version 2 del buscar local

--- compra_expres/src/app/shared/components/search/search.component.ts
+++ compra_expres/src/app/shared/components/search/search.component.ts
@@ -9,6 +9,26 @@ import { IonSearchbar } from '@ionic/angular/standalone';
 
 })
 export class SearchComponent  implements OnInit {
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public results = [...this.data];
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value?.toLowerCase() || '';
+    this.results = this.data.filter((d) => d.toLowerCase().includes(query));
+  }
+
 
   constructor() { }
 
