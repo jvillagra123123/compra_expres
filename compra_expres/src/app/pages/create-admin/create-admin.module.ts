f2df367b0b941f726f366548287b2816ec4a7221
BenjaminValle <148494222+BenjaminValle@users.noreply.github.com>
2025-01-17 15:23:27 -0300

    Pagina de crear local admin lista

--- compra_expres/src/app/pages/create-admin/create-admin.module.ts
+++ compra_expres/src/app/pages/create-admin/create-admin.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { CreateAdminPageRoutingModule } from './create-admin-routing.module';
-
 import { CreateAdminPage } from './create-admin.page';
+import { SharedModule } from'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    CreateAdminPageRoutingModule
+    CreateAdminPageRoutingModule,
+    SharedModule
   ],
   declarations: [CreateAdminPage]
 })
--- compra_expres/src/app/pages/create-admin/create-admin.page.ts
+++ compra_expres/src/app/pages/create-admin/create-admin.page.ts
@@ -4,12 +4,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-create-admin',
   templateUrl: './create-admin.page.html',
   styleUrls: ['./create-admin.page.scss'],
+  standalone: false,
 })
 export class CreateAdminPage implements OnInit {
+  newCategory: string = ''; // Variable para almacenar el texto ingresado
+  categories: string[] = []; // Arreglo para almacenar las categorías
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para añadir una nueva categoría
+  addCategory() {
+    if (this.newCategory.trim() !== '') {
+      this.categories.push(this.newCategory.trim()); // Agregar la categoría al arreglo
+      this.newCategory = ''; // Limpiar el input
+    }
   }
 
+  // Método para eliminar una categoría
+  removeCategory(index: number) {
+    this.categories.splice(index, 1); // Eliminar la categoría por índice
+  }
 }
