576206c09cd070858d2298c0a83aa01ce9e38ee0
TOPO0305 <127121279+TOPO0305@users.noreply.github.com>
2025-01-31 10:21:35 -0300

    borardo

--- compra_expres/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.CompraExpres.app',
-  appName: 'compra_expres',
-  webDir: 'www'
-};
-
-export default config;
